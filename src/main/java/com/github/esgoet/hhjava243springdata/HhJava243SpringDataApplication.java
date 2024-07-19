cb897f7d5030a12b671f30548020d64ff527c61c
esgoet <toddesgo@gmail.com>
2024-07-19 15:56:10 +0200

    Initial commit

---
+++ src/main/java/com/github/esgoet/hhjava243springdata/HhJava243SpringDataApplication.java
@@ -0,0 +1,13 @@
+package com.github.esgoet.hhjava243springdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhJava243SpringDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhJava243SpringDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/esgoet/hhjava243springdata/HhJava243SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.esgoet.hhjava243springdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhJava243SpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
